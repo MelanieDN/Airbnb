326f4fd60509ad951e5d5061d43d19796418ae4a
Mélanie NGUYEN <melaniedacosta0503@gmail.com>
2026-02-09 11:26:03 +0000

    adding the description of the intermediate tables for the documentation

---
+++ models/intermediate/customer_success/docs_price_category.md
@@ -0,0 +1,13 @@
+{% docs price_category %}
+
+The different prices have been regrouped in 5 categories :
+
+    - Budget : price below 80€
+    - Mid Range : price between 80€ and 150€
+    - Premium : price between 151€ and 300€
+    - Luxury : price between 301€ and 600€
+    - Ultra Luxury : price over 600€
+
+This categories were difined by combining the data distribution and the consumer psychology.
+
+{% enddocs %}
--- models/intermediate/customer_success/int_price_category.sql
+++ models/intermediate/customer_success/int_price_category.sql
@@ -5,10 +5,10 @@ SELECT
     price,
     CASE
         WHEN price < 80 THEN 'Budget'
-        WHEN price BETWEEN 80 AND 150 THEN 'Mid-Range'
+        WHEN price BETWEEN 80 AND 150 THEN 'Mid Range'
         WHEN price BETWEEN 151 AND 300 THEN 'Premium'
         WHEN price BETWEEN 301 AND 600 THEN 'Luxury'
-        WHEN price > 600  THEN 'Ultra_Luxury'
+        WHEN price > 600  THEN 'Ultra Luxury'
         ELSE NULL
     END AS price_category
 FROM {{ ref('stg_Airbnb__calendar') }}
